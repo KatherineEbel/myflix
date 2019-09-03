0c7907d66761089a72d04bd3bfc0f1fa96976043
Katherine Ebel <kathyebel@me.com>
2019-09-03 14:07:38 -0400

    Implement search_by_title feature for videos

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: 'videos#index'
   get '/home', to: 'videos#index'
-  resources :videos, only: %i[show]
+  resources :videos, only: %i[show] do
+    get 'search', on: :collection
+  end
   resources :categories, only: %i[show]
   get 'ui(/:action)', controller: 'ui'
 

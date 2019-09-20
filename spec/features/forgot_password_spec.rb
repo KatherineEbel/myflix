9758d23247effb2c9ea53be6a4214171d09f8607
Katherine Ebel <kathyebel@me.com>
2019-09-20 12:11:08 -0400

    use capybara-email gem to implement integration test for forgot password.

--- spec/features/forgot_password_spec.rb
+++ spec/features/forgot_password_spec.rb
@@ -1,13 +1,22 @@
 require 'rails_helper'
 
 feature 'Forgot Password' do
-  scenario 'user submits Forgot password' do
-    user = Fabricate(:user)
+  let(:user) { Fabricate(:user) }
+  background do
+    clear_emails
     visit root_path
     click_link 'Sign In'
     click_link 'Forgot Password?'
     fill_in 'Email address', with: user.email
     click_button 'Send Email'
-    expect(page).to have_content('Email sent')
+    open_email(user.email)
+  end
+  scenario 'following email button click' do
+    current_email.click_link 'Reset your password'
+    expect(page).to have_content('Reset Your Password')
+  end
+
+  scenario 'email has the content' do
+    expect(current_email).to have_content "Hi #{user.full_name}"
   end
 end

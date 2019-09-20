4718036bc16f5414be1acec30e2c4298e920a4f1
Katherine Ebel <kathyebel@me.com>
2019-09-20 09:57:28 -0400

    correct misplaced text in migration file

--- db/migrate/20190919153914_add_reset_password_token_to_user.rb
+++ db/migrate/20190919153914_add_reset_password_token_to_user.rb
@@ -1,6 +1,6 @@
 class AddResetPasswordTokenToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :reset_password_token, :stringanonymous_user
+    add_column :users, :reset_password_token, :string
     add_column :users, :reset_password_sent_at, :datetime
   end
 end
